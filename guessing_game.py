619ec13448e51b29bb386b30253205371cfdde9f
olee2 <65301179+olee2@users.noreply.github.com>
2020-05-15 20:15:55 +0200

    Create guessing_game.py

---
+++ guessing_game.py
@@ -0,0 +1,90 @@
+"""
+Python Web Development Techdegree
+Project 1 - Number Guessing Game
+--------------------------------
+"""
+
+import random
+
+
+high_score = 1000
+
+
+def correct_answer(number_of_tries):
+  
+  if number_of_tries == 1:
+    print("You got it on the first attempt. Excellent performance!\n")
+  
+  else: 
+    print("You got it on {} tries. Well done!\n".format(number_of_tries)) 
+
+
+def retry():
+  
+  play_again = ""
+
+  while play_again != "YES" or play_again != "NO":
+
+    play_again = input("Would you like to play again? YES / NO:  ")
+
+    if play_again.upper() == "YES":
+      main_game()
+
+    elif play_again.upper() == "NO":
+      print("The game is ending. Thank you for playing!")
+      exit()  
+      
+    else:
+      print("Please enter YES or NO!\n")
+
+      
+def main_game():
+  
+  global high_score
+  answer = random.randint(1, 50)
+  user_guess = 0
+  guess_count = 0
+  
+  if high_score <= 50:
+    print("\n--------> The current high score is {}! <--------\n".format(high_score))
+
+  while user_guess != answer:
+    
+    try:
+           
+      user_guess = input("Guess a number from 1-50: ")
+      user_guess = int(user_guess)
+      guess_count += 1
+    
+    except ValueError: 
+      
+      print("Please enter an integer from 1 - 50!\n")
+      continue
+    
+    else:
+      
+      if user_guess > 50:
+        print("You choose a value that was not within the range of numbers for this game.\n")
+        
+      elif user_guess < answer:
+        print("It is higher. Try again!\n")
+        
+      elif user_guess > answer:
+        print("It is lower. Try again!\n")
+      
+      else:
+        correct_answer(guess_count)
+  
+  if high_score > guess_count:
+    high_score = guess_count
+       
+
+def start_game():
+  
+  print("--------> Welcome to The Number Guessing Game! <--------")
+    
+  main_game()
+  
+  retry()
+                
+start_game()
